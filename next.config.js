32cbe26f8b20bfc3f6b327aacbac578347d589f6
Jianni Hu <jh2585@cornell.edu>
2023-07-09 15:43:52 -0400

    reinstall npm

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+module.exports = {
+  nextConfig,
+  env: {
+    STRAPI_URL: process.env.STRAPI_URL,
+  },
+};
