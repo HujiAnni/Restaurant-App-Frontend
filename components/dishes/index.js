421f37a8e6eaeeb36c6690458ab5dfd79a247d30
Jianni Hu <jh2585@cornell.edu>
2023-07-09 17:08:12 -0400

    adjusted button on homepage

--- components/dishes/index.js
+++ components/dishes/index.js
@@ -78,11 +78,7 @@ function Dishes({ restId }) {
                 <CardText>{res.attributes.description}</CardText>
               </CardBody>
               <div className="card-footer">
-                <Button
-                  color="secondary"
-                  outline
-                  onClick={() => appContext.addItem(res)}
-                >
+                <Button color="secondary" outline onClick={() => addItem(res)}>
                   + Add To Cart
                 </Button>
               </div>
