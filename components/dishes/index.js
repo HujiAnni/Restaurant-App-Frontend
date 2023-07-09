8d4c6bcf861d8a3c3fbffa06a519151550e62f7d
Jianni Hu <jh2585@cornell.edu>
2023-07-09 16:26:15 -0400

    adjusted dish image size

--- components/dishes/index.js
+++ components/dishes/index.js
@@ -70,7 +70,7 @@ function Dishes({ restId }) {
             <Card style={{ margin: "0 10px" }}>
               <CardImg
                 top={true}
-                style={{ height: 150, width: 150 }}
+                style={{ height: 150 }}
                 src={`http://localhost:1337${res.attributes.image.data.attributes.url}`}
               />
               <CardBody>
