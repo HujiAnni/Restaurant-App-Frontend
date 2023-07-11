1cefa09de41b94be2636aa305aa1628401d20014
Jianni Hu <jh2585@cornell.edu>
2023-07-10 22:01:25 -0400

    finetuned search function on restaurant page

--- pages/restaurant/[id].js
+++ pages/restaurant/[id].js
@@ -67,91 +67,124 @@ function Restaurants(props) {
     const searchQuery = restaurant.data.attributes.dishes.data.filter((obj) =>
       obj.attributes.name.toLowerCase().includes(query)
     );
-    return (
-      <>
-        <h1>{restaurant.data.attributes.name}</h1>
+    if (searchQuery.length) {
+      return (
+        <>
+          <h2>{restaurant.data.attributes.name}</h2>
 
-        <br></br>
-        {/* <Dishes search={query} /> */}
-        <div className="search">
-          <InputGroup>
-            <div className="input-group-append">
-              <span className="input-group-text" id="basic-addon2">
-                Search
-              </span>
-            </div>
-            <Input
-              onChange={(e) => updateQuery(e.target.value.toLocaleLowerCase())}
-              value={query}
-            />
-          </InputGroup>
-        </div>
+          <br></br>
+          {/* <Dishes search={query} /> */}
+          <div className="search">
+            <InputGroup>
+              <div className="input-group-append">
+                <span className="input-group-text" id="basic-addon2">
+                  Search
+                </span>
+              </div>
+              <Input
+                onChange={(e) =>
+                  updateQuery(e.target.value.toLocaleLowerCase())
+                }
+                value={query}
+              />
+            </InputGroup>
+          </div>
 
-        <br></br>
-        <Row>
-          {searchQuery.map((res) => {
-            let ress = { res };
-            // console.log(ress);
+          <br></br>
 
-            return (
-              <Col xs="6" sm="4" style={{ padding: 0 }} key={Math.random()}>
-                <Card style={{ margin: "0 10px" }}>
-                  <CardImg
-                    top={true}
-                    style={{ height: "100%" }}
-                    src={`${process.env.STRAPI_URL || "http://localhost:1337"}${
-                      res.attributes.image.data.attributes.url
-                    }`}
-                  />
-                  <CardBody>
-                    <CardTitle tag="h5">{res.attributes.name}</CardTitle>
-                    <CardText>{res.attributes.description}</CardText>
-                  </CardBody>
-                  <div className="card-footer">
-                    <Button
-                      outline
-                      color="secondary"
-                      onClick={() => {
-                        appContext.addItem(ress);
-                      }}
-                    >
-                      + Add To Cart
-                    </Button>
-                    <style jsx>
-                      {`
-                        a {
-                          color: white;
-                        }
-                        a:link {
-                          text-decoration: none;
-                          color: white;
-                        }
-                        .container-fluid {
-                          margin-bottom: 30px;
-                        }
-                        .btn-outline-primary {
-                          color: #007bff !important;
-                        }
-                        a:hover {
-                          color: white !important;
-                        }
-                      `}
-                    </style>
-                  </div>
-                </Card>
-              </Col>
-            );
-          })}
-          <Col xs="3" style={{ padding: 0 }}>
-            <div>
-              <Cart />
-            </div>
-          </Col>
-        </Row>
-      </>
-    );
+          <Row>
+            {searchQuery.map((res) => {
+              let ress = { res };
+              // console.log(ress);
+
+              return (
+                <Col xs="6" sm="4" style={{ padding: 0 }} key={Math.random()}>
+                  <Card style={{ margin: "0 10px" }}>
+                    <CardImg
+                      top={true}
+                      style={{ height: "100%" }}
+                      src={`${
+                        process.env.STRAPI_URL || "http://localhost:1337"
+                      }${res.attributes.image.data.attributes.url}`}
+                    />
+                    <CardBody>
+                      <CardTitle tag="h5">{res.attributes.name}</CardTitle>
+                      <CardText>{res.attributes.description}</CardText>
+                      <CardText>{res.attributes.price}</CardText>
+                    </CardBody>
+                    <div className="card-footer">
+                      <Button
+                        outline
+                        color="secondary"
+                        onClick={() => {
+                          appContext.addItem(ress);
+                        }}
+                      >
+                        + Add To Cart
+                      </Button>
+                      <style jsx>
+                        {`
+                          a {
+                            color: white;
+                          }
+                          a:link {
+                            text-decoration: none;
+                            color: white;
+                          }
+                          .container-fluid {
+                            margin-bottom: 30px;
+                          }
+                          .btn-outline-primary {
+                            color: #007bff !important;
+                          }
+                          a:hover {
+                            color: white !important;
+                          }
+                        `}
+                      </style>
+                    </div>
+                  </Card>
+                </Col>
+              );
+            })}
+            <Col xs="3" style={{ padding: 0 }}>
+              <div>
+                <Cart />
+              </div>
+            </Col>
+          </Row>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <h2>{restaurant.data.attributes.name}</h2>
+
+          <br></br>
+          {/* <Dishes search={query} /> */}
+          <div className="search">
+            <InputGroup>
+              <div className="input-group-append">
+                <span className="input-group-text" id="basic-addon2">
+                  Search
+                </span>
+              </div>
+              <Input
+                onChange={(e) =>
+                  updateQuery(e.target.value.toLocaleLowerCase())
+                }
+                value={query}
+              />
+            </InputGroup>
+          </div>
+
+          <br></br>
+          <h5>No Dishes Found</h5>
+        </>
+      );
+    }
   }
 
-  return <h1>Add Dishes</h1>;
+  return <h5>Add Dishes</h5>;
 }
 export default Restaurants;
