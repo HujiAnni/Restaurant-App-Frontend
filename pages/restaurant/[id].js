1b86485f220ec9a25e4eb13751c10f1497cfe0b0
Jennifer Hu <65553010+HujiAnni@users.noreply.github.com>
2023-08-04 13:32:30 -0400

    updated image fetching urls

--- pages/restaurant/[id].js
+++ pages/restaurant/[id].js
@@ -104,10 +104,7 @@ function Restaurants(props) {
                     <CardImg
                       top={true}
                       style={{ height: "100%" }}
-                      src={`${
-                        process.env.STRAPI_URL ||
-                        "https://restaurant-app-backend-strapi-d2f56ca44dd9.herokuapp.com"
-                      }${res.attributes.image.data.attributes.url}`}
+                      src={`${res.attributes.image.data.attributes.url}`}
                     />
                     <CardBody>
                       <CardTitle tag="h5">{res.attributes.name}</CardTitle>
